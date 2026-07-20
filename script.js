3f5dc86162614b92581b93f3316fffdea2ec44c5
Sourodip_18 <sourodipmahato762@gmail.com>
2026-07-20 10:49:14 +0530

    Modifying the file

--- script.js
+++ script.js
@@ -3,6 +3,15 @@
 
 function CreateCard(title, cName, views, monthsOld, duration, thumbnail){
     // making html variable
-    
+    let html = `        <div class="card">
+            <div class="image">
+                <img src="https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw"
+                    alt="">
+            </div>
+            <div class="text">
+                <h1>Introduction to Backend | Sigma Web Dev video #2</h1>
+                <p>CodewithHarry . 727k views . 2 months ago</p>
+            </div>
+        </div>`
 
 }
