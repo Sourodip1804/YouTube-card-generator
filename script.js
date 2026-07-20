3d6d0114ccd3901a61d756f2154472b520536d1b
Sourodip_18 <sourodipmahato762@gmail.com>
2026-07-20 12:56:28 +0530

    Modifying the file

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function createCard(title, cName, views, monthsOld, duration, thumbnail) {
     document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
 
 }
-
+ 
 // function call 
 
 createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
