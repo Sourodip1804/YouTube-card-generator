c41b2c3a34c17b51220e2e2d0119287ff2c7f70c
Sourodip_18 <sourodipmahato762@gmail.com>
2026-07-20 12:02:55 +0530

    Modifying the file

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 // to make this createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
 
 // ${title} to take any parameter or making a variable
-let viewStr
+
+
 function CreateCard(title, cName, views, monthsOld, duration, thumbnail){
+    if (views<1000000){
+        let viewStr
+
+    }
+
+
+
     // making html variable
     let html = `        <div class="card">
             <div class="image">
